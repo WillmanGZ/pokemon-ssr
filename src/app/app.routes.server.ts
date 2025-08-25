8d99be7852f8070081008ed6600acb8323b064b8
WillmanGZ <willmang@uninorte.edu.co>
2025-08-25 10:23:33 -0500

    feat: implement ssg for about, contact and pricing pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,9 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'about', renderMode: RenderMode.Prerender },
+  { path: 'pricing', renderMode: RenderMode.Prerender },
+  { path: 'contact', renderMode: RenderMode.Prerender },
   {
     path: '**',
     renderMode: RenderMode.Server,
