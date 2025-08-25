c6eabf3de9faacd5fd6f47c98910a91cbe6cb401
WillmanGZ <willmang@uninorte.edu.co>
2025-08-24 20:44:28 -0500

    feat: implement pokemon-page

---
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pokemons-page',
+  imports: [],
+  templateUrl: './pokemons-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class PokemonsPageComponent { }
