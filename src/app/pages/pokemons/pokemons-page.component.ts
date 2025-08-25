7573c870cd0a3cdc04c76ba7d4502cc1d082b5c1
WillmanGZ <willmang@uninorte.edu.co>
2025-08-24 22:53:53 -0500

    feat: implement pagination

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -10,6 +10,10 @@ import {
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces/simple-pokemon.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pokemons-page',
@@ -20,14 +24,34 @@ import { SimplePokemon } from '../../pokemons/interfaces/simple-pokemon.interfac
 export default class PokemonsPageComponent implements OnInit {
   private pokemonsService = inject(PokemonsService);
   public pokemons = signal<SimplePokemon[]>([]);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private title = inject(Title);
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map((params) => params.get('page') ?? '1'),
+      map((page) => (isNaN(+page) ? 1 : +page)),
+      map((page) => Math.max(1, page))
+    )
+  );
 
   ngOnInit(): void {
     this.loadPokemons();
   }
 
   public loadPokemons(page = 0) {
-    this.pokemonsService.loadPage(page).subscribe((pokemons) => {
-      this.pokemons.set(pokemons);
-    });
+    const pageToLoad = this.currentPage()! + page;
+
+    this.pokemonsService
+      .loadPage(pageToLoad)
+      .pipe(
+        tap(() =>
+          this.router.navigate([], { queryParams: { page: pageToLoad } })
+        ),
+        tap(() => this.title.setTitle(`Pokemons SSR - Page ${pageToLoad}`))
+      )
+      .subscribe((pokemons) => {
+        this.pokemons.set(pokemons);
+      });
   }
 }
