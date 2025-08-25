234c43345e8f184d1387542d60be3c8e6563b8de
WillmanGZ <willmang@uninorte.edu.co>
2025-08-25 10:35:47 -0500

    feat: prerender some pokemons pages

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,6 +5,26 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'pricing', renderMode: RenderMode.Prerender },
   { path: 'contact', renderMode: RenderMode.Prerender },
   {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [
+        { page: '1' },
+        { page: '2' },
+        { page: '3' },
+        { page: '4' },
+        { page: '5' },
+      ];
+    },
+  },
+  {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return [{ id: 'aerodactyl' }];
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server,
   },
